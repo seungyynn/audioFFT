6f4169e2feca17e16adc4f5c0cd17e649fac82aa
seungyynn <susan63177@khu.ac.kr>
2024-01-19 15:00:30 +0900

    ㅎㅇ

---
+++ dht11/Core/Inc/dht11.h
@@ -0,0 +1,15 @@
+/*
+ * dht11.h
+ *
+ *  Created on: Jan 15, 2024
+ *      Author: choiheawon2
+ */
+
+#ifndef INC_DHT11_H_
+#define INC_DHT11_H_
+
+#include "main.h"
+
+int dht11_read(void);
+
+#endif /* INC_DHT11_H_ */
---
+++ ssd1306/Core/Inc/hangulStringTable.h
@@ -0,0 +1,14 @@
+/*
+ * hangulStringTable.h
+ *
+ *  Created on: Jan 19, 2024
+ *      Author: npedu-203-admin
+ */
+
+#ifndef INC_HANGULSTRINGTABLE_H_
+#define INC_HANGULSTRINGTABLE_H_
+
+const char hello[] = {"임베디드"};
+const char string2[] = {"어려워"};
+
+#endif /* INC_HANGULSTRINGTABLE_H_ */
